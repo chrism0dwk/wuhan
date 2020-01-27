5a020551fbe2d28f7f960a5e441a1ae65ace246c
Chris Jewell <c.jewell@lancaster.ac.uk>
2020-01-27 16:17:53 +0000

    Added sensitivity analysis code.

---
+++ R/sens_analysis.R
@@ -0,0 +1,34 @@
+#' Sensitivity analysis for alpha
+
+par_init_ = c(0.4, 0.1428571, 1.0, 0.5)
+
+#' Fits the nCoV model
+#'
+#' @param alpha the assumed latent period
+#' @param K the flight connectivity matrix within China
+#' @param W the flight connectivity matrix internationally
+#' @param init_loc the initial city within China
+#' @param max_t the time up to which to make inference
+#' @return output from optim()
+#' @export
+fit_model = function(alpha=1/4, K=NA, W=NA, init_loc='Wuhan', max_t=22) {
+
+  simulator = NetworkODEModel(china_population, K, 'Wuhan', alpha, max_t)
+  llik = LogLikelihood(china_cases[,1:max_t], world_cases[,1:max_t],china_population, K, W, simulator)
+  p_hat = optim(log(par_init_), llik, control=list(fnscale=-1, maxit=2000))
+  p_hat
+}
+
+
+#' Sensitivity to alpha
+#'
+#' @param alpha a vector of alpha parameters to explore sensitivity to
+#' @param K the flight connectivity matrix within China
+#' @param W the flight connectivity matrix internationally
+#' @return a list of optim outputs and alphas
+#' @export
+alpha_sensitivity = function(alpha=c(3.6, 4.0, 4.4, 5.0, 6.0), K, W) {
+  res = lapply(alpha, function(a) fit_model(alpha=a, K=K, W=W))
+  res$alpha = alpha
+  res
+}
