400535cebb2e75f4fce1e26ff9b396276227459d
Chris Jewell <c.jewell@lancaster.ac.uk>
2020-02-09 15:50:48 +0000

    Removed detection probability from rest of world.

--- R/run_script.R
+++ R/run_script.R
@@ -16,8 +16,8 @@ run_script = function()
                        K_early=K_early/31,
                        K_late=K_late/31,
                        W_early=W_early/31,
-                       W_early=W_late/31,
-                       simulator=simulator)
+                       W_late=W_late/31,
+                       sim_fun=simulator)
   fit = optim(c(log(c(
     1.6, 1.6, 0.5, 0.5, 35
   )), logit(0.05)),
@@ -25,53 +25,83 @@ run_script = function()
   control = list(fnscale = -1),
   visualise = T)
 
+  params = c(exp(fit$par[1:5]), invlogit(fit$par[6]))
+
+
   # Predictive simulation
   simulator_pred = NetworkODEModel(
     china_population$Population,
-    K_early,
-    K_late,
+    K_early/31,
+    K_late/31,
     'Wuhan',
     alpha = 1 / 4,
     max_t = 365 * 2,
     t_control = 23
   )
-  sim = sim = simulator_pred(c(
-    1.45520920, 1.65177535, 1.18430698, 0.57174427, 510.71167077, 0.01638659
-  ))
+  sim = simulator_pred(params)
 
   # Infection curves
   par(mfrow=c(1,2))
   china_agg = aggregate_by_province(sim$I, china_population$Province)
-  plot(as.Date('2020-01-01')+sim$t[1:180], sim$I[1:180,151], type='l', main='Wuhan',
+  plot(as.Date('2020-01-01')+sim$t[1:220], sim$I[1:220,151], type='l', main='Wuhan',
        xlab='Date', ylab=expression(I(t)))
-  plot(as.Date('2020-01-01')+sim$t[1:180], rowSums(sim$I[1:180,]), main='China',
+  plot(as.Date('2020-01-01')+sim$t[1:220], rowSums(sim$I[1:220,]), main='China',
        xlab='Date', ylab=expression(I(t)), type='l')
 
   # Predictive plots
-  plot_pred_detections = function(sim, horizon) {
+  plot_pred_detections = function(sim, horizon, log=FALSE) {
     par(mfrow = c(2, 2))
     date_origin = as.Date('2020-01-01')
+
+    p_detect = rep(1, nrow(china_cases))
+    p_detect[151] = invlogit(fit$par[6])
+    expectedR = t(t(sim$R) * p_detect)
+
     china_prov = aggregate_by_province(t(china_cases), china_population$Province)
-    pred_prov = aggregate_by_province(sim$R, china_population$Province)
+    pred_prov = aggregate_by_province(expectedR, china_population$Province)
+
+    transform = identity
+    ylab = 'Cumulative detected'
+    if(isTRUE(log)) {
+      transform = log10
+      ylab = 'Log10 cumulative detected'
+    }
 
     prov_names = c('Hubei', 'Beijing', 'Guangdong', 'Shanghai')
     for (prov in prov_names) {
-      phi = ifelse(prov == 'Hubei', invlogit(fit$par[6]), 1)
       plot(
         date_origin + 1:35,
-        cumsum(china_prov[, prov]),
+        transform(cumsum(china_prov[, prov])),
         main = prov,
         pch = 20,
         xlab = 'Date',
-        ylab = 'Cumulative detected',
+        ylab = ylab,
         xlim = c(date_origin, date_origin + horizon),
-        ylim = c(0, max(pred_prov[1:horizon, prov] * phi))
+        ylim = c(0, max(transform(pred_prov[1:horizon, prov])))
       )
-      lines(date_origin + sim$t[1:horizon], pred_prov[1:horizon, prov] * phi, col =
-              2)
+      lines(date_origin + sim$t[1:horizon], transform(pred_prov[1:horizon, prov]), col=2)
     }
   }
 
   plot_pred_detections(sim, 40)
-  plot_pred_detections(sim, 365)
+  plot_pred_detections(sim, 365, log=T)
+
+
+  # Noise function for resampling
+  noise = NoiseGeneratingFunction(params, china_population$Population,
+                                  W_early, W_late)
+  llik1 = LogLikelihood(
+    y = noise$y,
+    z = noise$z,
+    pop_info = china_population,
+    K_early = K_early / 31,
+    K_late = K_late / 31,
+    W_early = W_early / 31,
+    W_late = W_late / 31,
+    sim_fun = simulator
+  )
+  fit_check = optim(fit$par,
+                    llik1,
+                    control = list(fnscale = -1),
+                    visualise = T)
 }
